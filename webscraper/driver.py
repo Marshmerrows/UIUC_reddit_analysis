a824f5378ea2306282428888b0bd127aa5a2b274
Jerry Li <jerryl3698@gmail.com>
2018-05-06 01:10:46 -0500

    prints first page to terminal now

--- webscraper/driver.py
+++ webscraper/driver.py
@@ -1,21 +1,34 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
-    driver = webdriver.Firefox()
+    driver = webdriver.Chrome()
     driver.get("https://www.reddit.com/r/UIUC/")
 
-    soup = bs(driver.page_source, 'html.parser')
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     for link in soup.find_all('a'):  # find all links on the current webpage, in order to filter
         href = str(link.get('href'))
         href_parts = href.split('/')
-        if len(href_parts) > 5 and href_parts[5] == 'comments':  # this link follows a post to its comment section
+        if len(href_parts) > 6 and href_parts[5] == 'comments':  # this link follows a post to its comment section
             driver.get(href)
-            try:
-                title = driver.find_element_by_class_name('content').get_attribute('innerHTML')
-                print title
-            except NoSuchElementException:
-                print "RIP no title"
+            for button in driver.find_elements_by_tag_name('button'):
+                if button.get_attribute('innerHTML') == 'back to Old Reddit.':
+                    button.click()
+                    break
+
+            thread_soup = BeautifulSoup(driver.page_source, 'html.parser')
+            post = thread_soup.find('div', id='siteTable')
+            comments = thread_soup.find('div', class_='commentarea').find_all('div', class_='md')
+
+            print post.find('p', class_='title').get_text()
+            print '--------------------------------'
+
+            print post.find('div', class_='usertext-body may-blank-within md-container ').get_text()
+
+            for comment in comments:
+                print comment.get_text()
+                print '--------------------------------'
+
     driver.quit()
