df0e0685f5bc3730f4263c43680f5322019c3c75
Jerry Li <jerryl3698@gmail.com>
2018-05-05 23:06:30 -0500

    Added some yung code bro

--- webscraper/driver.py
+++ webscraper/driver.py
@@ -1,21 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from bs4 import BeautifulSoup as bs 
-  
-COMMENT_LINK = 'https://www.reddit.com/comments/'
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup as bs
 
 if __name__ == "__main__":
     driver = webdriver.Firefox()
     driver.get("https://www.reddit.com/r/UIUC/")
 
     soup = bs(driver.page_source, 'html.parser')
-    for link in soup.find_all('a'): #find all links on the current webpage, in order to filter
-        href = str(link.get('href')) 
-        href_parts = href.split('/') 
-        if len(href_parts) > 5 and href_parts[5] == 'comments': #this link follows a post to its comment sectiion
+    for link in soup.find_all('a'):  # find all links on the current webpage, in order to filter
+        href = str(link.get('href'))
+        href_parts = href.split('/')
+        if len(href_parts) > 5 and href_parts[5] == 'comments':  # this link follows a post to its comment section
             driver.get(href)
-            source = driver.page_source
-            thread_soup = bs(source, 'html.parser')
-
-            print thread_soup.get_text()
+            try:
+                title = driver.find_element_by_class_name('content').get_attribute('innerHTML')
+                print title
+            except NoSuchElementException:
+                print "RIP no title"
     driver.quit()
